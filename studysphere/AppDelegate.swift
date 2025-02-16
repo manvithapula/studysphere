5aa216458902cfafda46e829d2cfbf8796bfcbb4
darkdeathoriginal <darkdeathoriginal@gmail.com>
2025-02-16 11:50:04 +0530

    style: changed navigation color to apptheme.primary

--- studysphere/AppDelegate.swift
+++ studysphere/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UINavigationBar.appearance().tintColor = .systemOrange
+        UINavigationBar.appearance().tintColor = AppTheme.primary
         FirebaseApp.configure()
         return true
     }
