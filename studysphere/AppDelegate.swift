b11ed5de32134cef607a680c734aba6f1017ec8f
darkdeathoriginal <darkdeathoriginal@gmail.com>
2025-03-08 16:17:52 +0530

    fix: Tabbar match apptheme

--- studysphere/AppDelegate.swift
+++ studysphere/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UINavigationBar.appearance().tintColor = AppTheme.primary
+        //change tabbar color
+        UITabBar.appearance().tintColor = AppTheme.primary
         FirebaseApp.configure()
         if #available(iOS 13.0, *) {
                     // Using SceneDelegate, do nothing here
